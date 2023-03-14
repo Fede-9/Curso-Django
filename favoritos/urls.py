4982efcc03216f4ca93fcaf502d7de5e92c535cf
FedeCometto <fedecometto98@gmail.com>
2023-03-14 20:47:32 -0300

    primeros cambios

---
+++ favoritos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+
+from .views import *
+
+app_name = 'favoritos'
+
+urlpatterns = [
+    path('', index_favoritos), #127.0.0.1:8000
+    path('crear', crear_favoritos)
+]
